d33f8618f72e3912040701f331d613cddb38df19
Mark <mark@hbar.co.uk>
2023-04-09 11:55:28 +0100

    Tailwind

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ const host = 'failand.me';
 
 export default defineConfig({
     server: {
+        host,
+        hmr: { host },
         https: {
             key: fs.readFileSync('./.cert/key.pem'),
             cert: fs.readFileSync('./.cert/cert.pem'),
