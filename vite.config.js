b50dc878aec19727de299e963268ad8c321942da
Mark <mark@hbar.co.uk>
2023-04-09 11:39:42 +0100

    server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
+
+const host = 'failand.me';
 
 export default defineConfig({
+    server: {
+        https: {
+            key: fs.readFileSync('./.cert/key.pem'),
+            cert: fs.readFileSync('./.cert/cert.pem'),
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
