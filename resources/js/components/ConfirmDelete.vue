2a20c1258bd85aac04376d767c4abcdcd6dc3c69
Mark <mark@hbar.co.uk>
2023-09-16 10:39:37 +0100

    delete fixed

--- resources/js/components/ConfirmDelete.vue
+++ resources/js/components/ConfirmDelete.vue
@@ -3,8 +3,8 @@
     <button class="inline-block bg-red-500 hover:bg-red-700 text-white px-4 py-2 rounded" @click="show = true"><slot></slot></button>
 </div>
 
-<div v-if="show" class="absolute left-0 top-0 min-w-full min-h-full bg-black opacity-50"></div>
-<div v-if="show" class="absolute left-0 top-0 min-w-full min-h-full">
+<div v-if="show" class="fixed left-0 top-0 min-w-full min-h-full bg-black opacity-50"></div>
+<div v-if="show" class="fixed left-0 top-0 min-w-full min-h-full">
     <form method="POST" :action="href" class="bg-white w-96 mx-auto mt-48 rounded-lg p-4">
         <input type="hidden" name="_method" value="DELETE">
         <input type="hidden" name="_token" :value="csrf">
