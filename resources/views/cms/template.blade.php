cfec9ba53a60402ca878a88923a8749837a1ad3b
Mark <mark@hbar.co.uk>
2023-10-02 10:39:10 +0100

    Cms page title

--- resources/views/cms/template.blade.php
+++ resources/views/cms/template.blade.php
@@ -10,13 +10,14 @@
 </head>
 <body class="bg-lime-700">
 <div id="app" class="w-3/5 mx-auto min-h-screen bg-white p-4">
-    <nav class="flex flex-row mb-8 text-4xl justify-between">
+    <nav class="flex flex-row mb-4 text-4xl justify-between">
         <a href="/cms" @if(Request::path() == 'cms') class="text-lime-700 underline" @endif>CMS</a>
         <div class="flex flex-row grow justify-center space-x-8">
             <a href="/cms/pages" @if(Request::path() == 'cms/pages') class="text-lime-700 underline" @endif>Pages</a>
             <a href="/cms/images" @if(Request::path() == 'cms/images') class="text-lime-700 underline" @endif>Images</a>
         </div>
     </nav>
+    <h1 class="text-lime-700 text-4xl mb-8">@yield('title')</h1>
     <div>
         @yield('content')
     </div>
