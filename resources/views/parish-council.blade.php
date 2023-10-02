3015372d6ae10576a5d84aed28070b9025a19d0b
Mark <mark@hbar.co.uk>
2023-10-02 12:36:59 +0100

    meetings

--- resources/views/parish-council.blade.php
+++ resources/views/parish-council.blade.php
@@ -1,4 +1,4 @@
 <h2 class="text-xl mb-2">Parish Council Meetings</h2>
 
-<div>Thursday 12 October 2023 at 7.30pm via Zoom</div>
+<div>Thursday 12 October 2023 at 7.30pm in the Cross Tree Centre behind Wraxall church</div>
 
