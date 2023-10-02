858c29521e75ceee45dbc5ebce654763bb8b4189
Mark <mark@hbar.co.uk>
2023-10-02 09:36:19 +0100

    cms nav

--- resources/views/cms/template.blade.php
+++ resources/views/cms/template.blade.php
@@ -9,9 +9,15 @@
   @vite('resources/js/app.js')
 </head>
 <body class="bg-lime-700">
-<div id="app" class="w-3/5 mx-auto border min-h-screen bg-white">
-    <h1 class="text-4xl text-lime-700 text-center mb-4">@yield('title')</h1>
-    <div class="p-4">
+<div id="app" class="w-3/5 mx-auto min-h-screen bg-white p-4">
+    <nav class="flex flex-row mb-8 text-4xl justify-between">
+        <a href="/cms" @if(Request::path() == 'cms') class="text-lime-700 underline" @endif>CMS</a>
+        <div class="flex flex-row grow justify-center space-x-8">
+            <a href="/cms/pages" @if(Request::path() == 'cms/pages') class="text-lime-700 underline" @endif>Pages</a>
+            <a href="/cms/images" @if(Request::path() == 'cms/images') class="text-lime-700 underline" @endif>Images</a>
+        </div>
+    </nav>
+    <div>
         @yield('content')
     </div>
 </div>
